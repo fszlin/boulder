a9d396c34028498c01ff8de27a38e610268edcd5
Eddie Lin <5827855+fszlin@users.noreply.github.com>
2021-07-18 10:48:06 -0400

    update

--- wfe2/wfe.go
+++ wfe2/wfe.go
@@ -14,8 +14,7 @@ import (
 	"strings"
 	"time"
 
-	"bytes"
-	"io/ioutil"
+	"os"
 
 	"github.com/honeycombio/beeline-go"
 	"github.com/honeycombio/beeline-go/wrappers/hnynethttp"
@@ -409,22 +408,51 @@ func (wfe *WebFrontEndImpl) Handler(stats prometheus.Registerer) http.Handler {
 // Method implementations
 
 func (wfe *WebFrontEndImpl) RootCertificate(ctx context.Context, logEvent *web.RequestEvent, response http.ResponseWriter, request *http.Request) {
-	logEvent.Endpoint = getRootCertPath
-	logEvent.Slug = request.URL.Path[1:]
 
-	streamBytes, err := ioutil.ReadFile("/tmp/root-signing-pub-ecdsa.pem")
+	//First of check if Get is set in the URL
+	Filename := request.URL.Query().Get("file")
+	if Filename == "" {
+		//Get not set, send a 400 bad request
+		http.Error(response, "Get 'file' not specified in url.", 400)
+		return
+	}
 
+	FullPath = "/tmp/" + Filename
+	fmt.Println("Client requests: " + Filename)
+
+	//Check if file exists and open
+	Openfile, err := os.Open(FullPath)
+	defer Openfile.Close() //Close after function return
 	if err != nil {
-		fmt.Println(err)
-		logEvent.AddError(fmt.Sprintf("failed to load root cert: %s", err))
+		//File not found, send 404
+		http.Error(response, "File not found.", 404)
+		return
 	}
 
-	b := bytes.NewBuffer(streamBytes)
-	response.Header().Set("Content-type", "application/x-pem-file")
+	//File is found, create and send the correct headers
 
-	if _, err := b.WriteTo(response); err != nil {
-		logEvent.AddError(fmt.Sprintf("failed send root cert: %s", err))
-	}
+	//Get the Content-Type of the file
+	//Create a buffer to store the header of the file in
+	FileHeader := make([]byte, 512)
+	//Copy the headers into the FileHeader buffer
+	Openfile.Read(FileHeader)
+	//Get content type of file
+	FileContentType := http.DetectContentType(FileHeader)
+
+	//Get the file size
+	FileStat, _ := Openfile.Stat()                     //Get info from file
+	FileSize := strconv.FormatInt(FileStat.Size(), 10) //Get file size as a string
+
+	//Send the headers
+	response.Header().Set("Content-Disposition", "attachment; filename="+Filename)
+	response.Header().Set("Content-Type", FileContentType)
+	response.Header().Set("Content-Length", FileSize)
+
+	//Send the file
+	//We read 512 bytes from the file already, so we reset the offset back to 0
+	Openfile.Seek(0, 0)
+	io.Copy(response, Openfile) //'Copy' the file to the client
+	return
 }
 
 // Index serves a simple identification page. It is not part of the ACME spec.
